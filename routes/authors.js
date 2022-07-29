adb847f775335df56e32ec3adf0895846749387f
WilltheGreat098 <william.roberts2313@gmail.com>
2022-07-30 08:56:24 +1200

    Initial Author Routes

--- routes/authors.js
+++ routes/authors.js
@@ -35,7 +35,7 @@ router.post('/', async (req, res) => {
         const newAuthor = await author.save()
         // res.redirect('authors/${newAuthor.id}')
         res.redirect('authors')
-    } catch {
+    } catch (error) {
         res.render('authors/new', {
             author: author,
             errorMessage: "Error creating Author"
